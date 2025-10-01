7db3bbc366ceb2380de70c79a753d8bc3fb0a74e
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-10-01 10:10:03 -0400

    Add functions and tests

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 def add(x, y):
     return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ZeroDivisionError
+    return x / y
--- test_main.py
+++ test_main.py
@@ -1,6 +1,31 @@
-from main import add
+from main import add, subtract, divide
 
-def test_add():
+import pytest
+
+def test_add_positive_integers():
     result = add(1, 2)
+    expected = 3
     
-    assert result == 3
+    assert result == expected
+
+def test_add_negative_integers():
+    result = add(-4, -9)
+    expected = -13
+
+    assert result == expected
+
+
+def test_subtract():
+    result = subtract(3, 1)
+    expected = 2
+
+    assert result == expected
+
+def test_divide():
+    result = divide(10, 2)
+
+    assert result == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        result = divide(1, 0)
